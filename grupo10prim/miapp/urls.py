8d10bf975fc9c6490ce2e98170623fd82d0aeca1
Carolina Paz Salgado Parraguez <carolinasalgado@ug.uchile.cl>
2019-09-13 16:03:05 -0300

    se agrega nabar cliente y navbar empresa

--- grupo10prim/miapp/urls.py
+++ grupo10prim/miapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-	path('', index, name ='index'),
+	path('', homeCliente, name ='homeCliente'),
 	path('inputs/', inputs, name='inputs'),
 	path("mostrar_resultado", recuperar, name='mostrar_resultado'),
 	path('contacto', contacto, name='contacto'),
