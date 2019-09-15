d6ea4f9768ee1b4c7779bff58734508ebbed6eaf
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-09-15 00:51:31 -0300

    started user app

--- grupo10prim/miapp/views.py
+++ grupo10prim/miapp/views.py
@@ -1,69 +1,69 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def homeCliente(request):
-	return render(request, 'homeCliente.html')
+    return render(request, 'homeCliente.html')
+
 
 def inputs(request):
-	return render(request, 'inputs.html')
+    return render(request, 'inputs.html')
+
 
 def contacto(request):
-	return render(request, 'contacto.html')
+    return render(request, 'contacto.html')
+
 
 def agenda(request):
-	return render(request, 'agenda.html')
+    return render(request, 'agenda.html')
+
 
 def catalogo(request):
-	return render(request, 'catalogo.html')
-
+    return render(request, 'catalogo.html')
 
 
 def backoffice(request):
-	return render(request, 'backoffice.html')
+    return render(request, 'backoffice.html')
+
 
 def homeEmpresa(request):
-	return render(request, 'homeEmpresa.html')
+    return render(request, 'homeEmpresa.html')
+
 
 def fichaClinica(request):
-	return render(request, 'fichaClinica.html')
+    return render(request, 'fichaClinica.html')
+
 
 def estadisticas(request):
-	return render(request, 'estadisticas.html')
+    return render(request, 'estadisticas.html')
+
 
 def inventario(request):
-	return render(request, 'inventario.html')
+    return render(request, 'inventario.html')
+
 
 def comprarObjeto(request):
-        return render(request,'comprarObjeto.html')
+    return render(request, 'comprarObjeto.html')
 
 
-					
 def recuperar(request):
-	diccionario={}
+    diccionario = {}
 
-	name1 = request.POST.get("inputComida1")
-	if (name1):
-		diccionario["Bebestible"] = "Bebestible"
+    name1 = request.POST.get("inputComida1")
+    if (name1):
+        diccionario["Bebestible"] = "Bebestible"
 
-	name2 = request.POST.get("inputComida2")
-	if (name2):
-		diccionario["Snack"] = "Snack"
+    name2 = request.POST.get("inputComida2")
+    if (name2):
+        diccionario["Snack"] = "Snack"
 
-	name3 = request.POST.get("inputComida3")
-	if (name3):
-		diccionario["Cena"] = "Cena"
+    name3 = request.POST.get("inputComida3")
+    if (name3):
+        diccionario["Cena"] = "Cena"
 
-	
-	nombre=request.POST["inputNombre"]
-	edad=request.POST["inputEdad"]
-	email=request.POST["inputEmail"]
-	carrera=request.POST["exampleRadios"]
-	tema=request.POST["inputTema"]
+    edad = request.POST["inputEdad"]
+    email = request.POST["inputEmail"]
 
-	diccionario["Nombre"]=nombre
-	diccionario["Edad"]=edad
-	diccionario["Email"]=email
-	diccionario["Departamento"]=carrera
-	diccionario["Tematica"]=tema
+    diccionario["Edad"] = edad
 
-	return render(request, "mostrar_resultado.html", diccionario)
+    return render(request, "mostrar_resultado.html", diccionario)
